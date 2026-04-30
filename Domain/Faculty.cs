5f181a1282b73d9386a251289c46120e258c68d2
Carolina <181431400+carolinahrnd3z@users.noreply.github.com>
2026-04-29 23:05:24 -0400

    Update Faculty.cs

--- Domain/Faculty.cs
+++ Domain/Faculty.cs
@@ -3,24 +3,22 @@ namespace ITSC_3112_Final_Project.Domain;
 public class Faculty : Person
 { 
     public int EmployeeId { get; set; }
-    private List<Member> members = new List<Member>();
     
     public Faculty(string name, string email, string phoneNumber, int employeeId) : base(name, email, phoneNumber)
     {
         EmployeeId = employeeId;
     }
 
-    public Member? SearchMember(string value)
+    public Member? SearchMember(string value, List<Member> members)
     {
         return members.FirstOrDefault(m => m.Name.Equals(value, StringComparison.OrdinalIgnoreCase) || m.Email.Equals(value, StringComparison.OrdinalIgnoreCase) || m.AccountNumber.ToString() == value);
     }
 
     void UpdateMemberInfo(Member member)
     {
-            existing.Name = member.Name;
-            existing.Email = member.Email;
-            existing.PhoneNumber = member.PhoneNumber;
-            existing.MembershipType = member.MembershipType;
-        }
+            member.Name = member.Name;
+            member.Email = member.Email;
+            member.PhoneNumber = member.PhoneNumber;
+            member.MembershipType = member.MembershipType;
     }
 }
