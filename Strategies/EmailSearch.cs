75a77ec310cc8a9d579faa5da48885e0d1fe8623
HezekiahTetterton <htettert@charlotte.edu>
2026-05-01 10:28:50 -0400

    Create EmailSearch.cs

---
+++ Strategies/EmailSearch.cs
@@ -0,0 +1,12 @@
+using ITSC_3112_Final_Project.Domain;
+using ITSC_3112_Final_Project.Repositories;
+
+namespace ITSC_3112_Final_Project.Strategies;
+
+public class EmailSearch : SearchStrategy
+{
+    public override Member? Search(MemberRepository repository, string value)
+    {
+        return repository.GetByEmail(value);
+    }
+}
