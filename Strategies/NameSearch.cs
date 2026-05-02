7b441a9c44b91271b1b1d36e3f79b3e1dc321ed3
HezekiahTetterton <htettert@charlotte.edu>
2026-05-02 11:27:35 -0400

    Update NameSearch.cs

--- Strategies/NameSearch.cs
+++ Strategies/NameSearch.cs
@@ -5,7 +5,7 @@ namespace ITSC_3112_Final_Project.Strategies;
 
 public class NameSearch : SearchStrategy
 {
-    public override Member? Search(MemberRepository repository, string value)
+    public override Member? Search(IMemberRepository repository, string value)
     {
         return repository.GetByName(value);
     }
