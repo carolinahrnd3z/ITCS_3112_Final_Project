a11c046fb193b0f2a32d555fce67d1024feec6cc
HezekiahTetterton <htettert@charlotte.edu>
2026-05-02 11:27:55 -0400

    Update AccountNumberSearch.cs

--- Strategies/AccountNumberSearch.cs
+++ Strategies/AccountNumberSearch.cs
@@ -5,7 +5,7 @@ namespace ITSC_3112_Final_Project.Strategies;
 
 public class AccountNumberSearch : SearchStrategy
 {
-    public override Member? Search(MemberRepository repository, string value)
+    public override Member? Search(IMemberRepository repository, string value)
     {
         if (!int.TryParse(value, out int accountNumber))
             return null;
