81c52d845490695043fe0e03f246bf7f73504cfd
HezekiahTetterton <htettert@charlotte.edu>
2026-05-02 18:47:35 -0400

    Add files via upload

---
+++ Services/ILoginService.cs
@@ -0,0 +1,8 @@
+﻿using ITSC_3112_Final_Project.Domain;
+
+namespace ITCS_3112_Final_Project.Services;
+
+public interface ILoginService
+{
+    Faculty? Login(string username, string password);
+}
--- Services/LoginService.cs
+++ Services/LoginService.cs
@@ -1,1 +1,23 @@
+﻿using ITSC_3112_Final_Project.Domain;
+using ITSC_3112_Final_Project.Repositories;
 
+namespace ITCS_3112_Final_Project.Services;
+
+public class LoginService : ILoginService
+{
+    private readonly IFacultyRepository facultyRepository;
+
+    public LoginService(IFacultyRepository facultyRepository)
+    {
+        this.facultyRepository = facultyRepository;
+    }
+
+    public Faculty? Login(string username, string password)
+    {
+        var facultyList = facultyRepository.GetAll();
+
+        return facultyList.FirstOrDefault(f =>
+            f.Username == username &&
+            f.Password == password);
+    }
+}
