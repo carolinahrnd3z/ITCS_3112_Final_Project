9255c39f9ac5908104c465519b2294aff306f991
Carolina <181431400+carolinahrnd3z@users.noreply.github.com>
2026-05-02 20:58:54 -0400

    Updated gym class types

--- Domain/StrengthClass.cs
+++ Domain/StrengthClass.cs
@@ -1,6 +1,12 @@
 namespace ITSC_3112_Final_Project.Domain;
 
-public class StrengthClass
+public class StrengthClass : GymClass
 {
-    
+    public StrengthClass(string name, string schedule, int capacity, string instructor) : base(name, schedule, capacity, instructor)
+    {}
+
+    public override bool ApplyClassRules(Member member)
+    {
+        return member.MembershipType == MembershipEnum.PRO || member.MembershipType == MembershipEnum.ELITE;
+    }
 }
--- Domain/ZumbaClass.cs
+++ Domain/ZumbaClass.cs
@@ -1,6 +1,11 @@
 namespace ITSC_3112_Final_Project.Domain;
 
-public class ZumbaClass
+public class ZumbaClass : GymClass
 {
+    public YogaClass(string name, string schedule, int capacity, string instructor) : base(name, schedule, capacity, instructor){}
     
+    protected override bool ApplyClassRules(Member member)
+    {
+        return member.MembershipType != MembershipEnum.BASIC;
+    }
 }
